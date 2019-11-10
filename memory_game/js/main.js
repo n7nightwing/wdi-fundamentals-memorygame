1414f9141e260e170b041c1a4da088d2e7c297b5
Arty Barnabi <rbarnabi676@gmail.com>
2019-11-09 22:08:46 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,28 @@
 console.log("Up and running!");
 
 const cards = ["Queen", "Queen", "King", "King"];
-
 const cardsInPlay = [];
 
-var cardOne = cards[3];
-var cardTwo = cards[2];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		//say "it's a match!"
+	    alert("You found a match!");
+	else
+		alert("Sorry, try again.");
+}
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	if (cardsInPlay.length === 2)
+		checkForMatch()
 
+			//say "not a match :c"
+}
+
+flipCard(0);
+flipCard(2);
+/*
 if (cardsInPlay.length === 2)
 	if (cardsInPlay[0] === cardsInPlay[1])
 		//say "it's a match!"
@@ -19,3 +30,6 @@ if (cardsInPlay.length === 2)
 	else
 		alert("Sorry, try again.");
 		//say "not a match :c"
+*/
+
+
